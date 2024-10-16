82013ab5365d71a51116a782d33e8870269b2f2f
Satyajeet-Das <dassatyajeet123@gmail.com>
2024-10-16 20:02:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { DiscordRequest, VerifyDiscordRequest } from './utils.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}))
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+client.login(process.env.DISCORD_TOKEN)
+
+app.post('/interactions', async (req, res) => {
+    const {type, id, data} = req.body;
+
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
